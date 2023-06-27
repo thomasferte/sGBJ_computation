5085aad9246cbcb345b3e0aefc8f0ba1ee3989a4
Thomas Ferte <thomas.ferte@u-bordeaux.fr>
2023-06-27 18:27:15 +0200

    add rf method

--- functions/fct_simulation_paper.R
+++ functions/fct_simulation_paper.R
@@ -14,7 +14,8 @@
 #'
 #' @return A dataframe with the different p-values
 #' @export
-fct_simulation_paper <- function(nb_observations = 50,
+fct_simulation_paper <- function(methods = c("sgbj", "gbt", "other", "rf"),
+                                 nb_observations = 50,
                                  nb_genes = 50,
                                  prop_sig_gene = 0.2,
                                  variance = 0.2,
@@ -25,6 +26,14 @@ fct_simulation_paper <- function(nb_observations = 50,
                                  prop_two_periods = FALSE,
                                  nperm_sGBJ = 300,
                                  slam = 0.005) {
+  ls_pvalue <-
+    list(
+      "sGBJ" = NA,
+      "Global Test" = NA,
+      "Wald Test" = NA,
+      "Global Boost Test" = NA,
+      "RF" = NA
+    )
   ##### generate data #####
   dfdata <- fct_generate_data(
     case = case,
@@ -41,56 +50,78 @@ fct_simulation_paper <- function(nb_observations = 50,
   )
   
   ##### sGBJ #####
-  resSGBJ <-
-    sGBJ::sGBJ(
-      surv = survival::Surv(dfdata$time, dfdata$event),
-      factor_matrix = dfdata %>% select(-time,-event) %>% as.matrix(),
-      nperm = nperm_sGBJ
-    )
+  if("sgbj" %in% methods){
+    resSGBJ <-
+      sGBJ::sGBJ(
+        surv = survival::Surv(dfdata$time, dfdata$event),
+        factor_matrix = dfdata %>% select(-time,-event) %>% as.matrix(),
+        nperm = nperm_sGBJ
+      )$sGBJ_pvalue
+    
+    ls_pvalue$sGBJ <- resSGBJ
+  }
+  
   #Global boost test
-  gbst <-
-    globalboosttest::globalboosttest(
-      X = dfdata %>% select(-time,-event) %>% as.matrix(),
-      Y = survival::Surv(dfdata$time, dfdata$event),
-      Z = NULL,
-      nperm = nb_permutation,
-      mstop = 500,
-      pvalueonly = TRUE
-    )
+  if("gbt" %in% methods){
+    gbst <-
+      globalboosttest::globalboosttest(
+        X = dfdata %>% select(-time,-event) %>% as.matrix(),
+        Y = survival::Surv(dfdata$time, dfdata$event),
+        Z = NULL,
+        nperm = nb_permutation,
+        mstop = 500,
+        pvalueonly = TRUE
+      )$pvalue[1]
+    
+    ls_pvalue$`Global Boost Test` <- gbst
+  }
   
   ##### Other methods #####
-  ots <- statsTest(
-    vectime = dfdata$time,
-    vecevent = dfdata$event,
-    x = dfdata %>% select(-time,-event) %>% as.matrix()
-  )
-  rts <- matrix(0, nrow = 2, ncol = nb_permutation)
-  
-  for (b in 1:nb_permutation) {
-    if (b %% 100 == 0) {
-      print(b)
-    }
-    
-    seed <- as.numeric(Sys.time()) + b
-    
-    set.seed(seed)
-    
-    dfdatapermuted <- dfdata %>%
-      dplyr::select(time, event) %>%
-      dplyr::slice_sample(prop = 1, replace = TRUE)
-    
-    rts[, b] <- statsTest(
-      vectime = dfdatapermuted$time,
-      vecevent = dfdatapermuted$event,
+  if("other" %in% methods){
+    ots <- statsTest(
+      vectime = dfdata$time,
+      vecevent = dfdata$event,
       x = dfdata %>% select(-time,-event) %>% as.matrix()
     )
+    rts <- matrix(0, nrow = 2, ncol = nb_permutation)
+    
+    for (b in 1:nb_permutation) {
+      if (b %% 100 == 0) {
+        print(b)
+      }
+      
+      seed <- as.numeric(Sys.time()) + b
+      
+      set.seed(seed)
+      
+      dfdatapermuted <- dfdata %>%
+        dplyr::select(time, event) %>%
+        dplyr::slice_sample(prop = 1, replace = FALSE)
+      
+      rts[, b] <- statsTest(
+        vectime = dfdatapermuted$time,
+        vecevent = dfdatapermuted$event,
+        x = dfdata %>% select(-time,-event) %>% as.matrix()
+      )
+    }
+    vec_pvalue <- apply((rts > ots), 1, mean)
+    
+    ls_pvalue$`Global Test` <- vec_pvalue[1]
+    ls_pvalue$`Wald Test` <- vec_pvalue[2]
   }
-  p.pvalue <- c(resSGBJ$sGBJ_pvalue, apply((rts > ots), 1, mean), as.numeric(gbst$pvalue))
+  
+  if("rf" %in% methods){
+    p_val_rf <- rfpermutation(dfdata = dfdata, nb_permutation = nb_permutation)
+    
+    ls_pvalue$RF <- p_val_rf
+  }
   
   res <- data.frame(
-    p_value = p.pvalue,
-    method = c("sGBJ", "Global Test", "Wald Test", "Global Boost Test")
-  )
+    p_value = ls_pvalue %>% unlist(),
+    method = names(ls_pvalue)
+  ) %>%
+    na.omit() %>%
+    tibble::remove_rownames()
   
   return(res)
 }
---
+++ functions/rf_permutation.R
@@ -0,0 +1,46 @@
+#' rfpermutation
+#'
+#' @description Function which compute test statistics for rf with permutation
+#'
+#' @param vectime the time
+#' @param vecevent the event
+#' @param x the genes experession
+#' @param nb_permutation the number of permutation
+#'
+#' @return A vector with the statistical test of global test, adewale test
+#' @export
+#'
+rfpermutation=function(dfdata, nb_permutation = 10, ntree = 10){
+  
+  model <- randomForestSRC::rfsrc(Surv(time,event) ~ .,
+                                  data = dfdata,
+                                  ntree = ntree,
+                                  importance = FALSE)
+  oob_error <- model$err.rate[ntree]
+  
+  vec_ooberror <- pbapply::pblapply(X = 1:nb_permutation,
+                                   FUN = function(b){
+                                     seed <- as.numeric(Sys.time()) + b
+                                     
+                                     set.seed(seed)
+                                     
+                                     dfdatapermuted <- dfdata %>%
+                                       dplyr::select(time, event) %>%
+                                       dplyr::slice_sample(prop = 1, replace = FALSE) %>%
+                                       bind_cols(dfdata %>% select(-c(time, event)))
+                                     
+                                     model <- randomForestSRC::rfsrc(Surv(time,event) ~ .,
+                                                                     data = dfdatapermuted,
+                                                                     ntree = ntree,
+                                                                     importance = FALSE)
+                                     oob_error <- model$err.rate[ntree]
+                                     
+                                     return(oob_error)
+                                   }) %>%
+    unlist()
+  
+    
+  p.pvalue <- mean(oob_error < vec_ooberror)
+  
+  return(ts)
+}
