ca416f195005634d0e77f7c6db7c85ac3ce99cba
Thomas Ferte <thomas.ferte@u-bordeaux.fr>
2025-03-12 16:30:19 +0100

    adapt old scripts

--- script/breast_cancer/05_master_analysis_breast.R
+++ script/breast_cancer/05_master_analysis_breast.R
@@ -9,16 +9,25 @@ library(survminer)
 library(parallel)
 
 ##### survival analysis #####
-lsdata <- readRDS(file = "data/breast_cancer/datamanaged.rds")
+lsdata <- readRDS(file = "data/breast_cancer/datamanaged2.rds")
 
 lapply(X = list.files("functions/", full.names = T), source)
 
 ls_pathways <- lsdata$pathways
 df_data <- lsdata$data
 
+##### get pathway
+vec_pathway <- unique(lsdata$pathways$gs_name)
+pathway_taskid <- vec_pathway[taskid]
+gene_taskid <- lsdata$pathways |> 
+  filter(gs_name == pathway_taskid) |> 
+  pull(gene_symbol)
+
+ls_pathway_taskid <- setNames(list(gene_taskid), pathway_taskid)
+
 ##### analysis
 df_analysis_breast <- fct_breast_cancer_analysis(df_data = df_data,
-                                                 ls_pathways = ls_pathways[taskid],
+                                                 ls_pathways = ls_pathway_taskid,
                                                  vec_covariates = "age",
                                                  nb_permutation = 1000)
 
